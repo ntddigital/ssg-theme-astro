42a58e65c7e06cb200f8ed5bee0d940d2bdb9c42
HainingCui <92837364+HainingCui@users.noreply.github.com>
2024-08-21 13:27:40 +0900

    feature: structured data etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  // site: 'https://example.com',
   integrations: [tailwind(), react(), mdx()],
   srcDir: './demo'
 });
